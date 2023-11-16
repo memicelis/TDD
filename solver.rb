518be4cf63afe24624a84c978f81faa8b12b3a22
memicelis <elismemic5@gmail.com>
2023-11-16 14:37:45 +0100

    Fix Rubocop linters

--- solver.rb
+++ solver.rb
@@ -1,27 +1,27 @@
 class Solver
-    def factorial(n)
-        if(n<0)
-            raise ArgumentError, "Input must be non-negative integer"
-        elsif (n == 0)
-        1
-        else
-            n*factorial(n-1)
-        end
+  def factorial(num)
+    if num.negative?
+      raise ArgumentError, 'Input must be non-negative integer'
+    elsif num.zero?
+      1
+    else
+      num * factorial(n - 1)
     end
+  end
 
-    def reverse(word)
-        word.reverse
-    end
+  def reverse(word)
+    word.reverse
+  end
 
-    def fizzbuzz(n)
-        if n%3 == 0 && n%5 ==0
-            "fizzbuzz"
-        elsif n%3 == 0
-            "fizz"
-        elsif n%5 == 0
-            "buzz"
-        else
-            n.to_s
-        end
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'fizzbuzz'
+    elsif (num % 3).zero?
+      'fizz'
+    elsif (num % 5).zero?
+      'buzz'
+    else
+      num.to_s
     end
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,40 +2,39 @@ require 'rspec'
 require_relative '../solver'
 
 describe Solver do
-
-    describe "#factorial" do
-        it 'returns 1 for factorial of 0' do
-            expect(subject.factorial(0)).to eq(1)
-        end
-
-        it "returns ArgumentError for negative integers" do
-            expect{subject.factorial(-2)}.to raise_error(ArgumentError, "Input must be non-negative integer")
-        end
-
-        it "returns correct factorial for positive integer" do
-            expect(subject.factorial(1)).to eq(1)
-            expect(subject.factorial(5)).to eq(120)
-        end
+  describe '#factorial' do
+    it 'returns 1 for factorial of 0' do
+      expect(subject.factorial(0)).to eq(1)
     end
 
-    describe "#reverse" do
-        it "returns word reversed" do
-            expect(subject.reverse("hello")).to eq("olleh")
-        end
+    it 'returns ArgumentError for negative integers' do
+      expect { subject.factorial(-2) }.to raise_error(ArgumentError, 'Input must be non-negative integer')
     end
 
-    describe "#fizzbuzz" do
-        it "returns fizz when is n is divisible by 3" do
-            expect(subject.fizzbuzz(3)).to eq("fizz")
-        end
-        it "returns buzz when is n is divisible by 5" do
-            expect(subject.fizzbuzz(5)).to eq("buzz")
-        end
-        it "returns fizz when is n is divisible by 3 and 5" do
-            expect(subject.fizzbuzz(15)).to eq("fizzbuzz")
-        end
-        it "return n as string " do
-            expect(subject.fizzbuzz(7)).to eq("7")
-        end
+    it 'returns correct factorial for positive integer' do
+      expect(subject.factorial(1)).to eq(1)
+      expect(subject.factorial(5)).to eq(120)
     end
-end 
+  end
+
+  describe '#reverse' do
+    it 'returns word reversed' do
+      expect(subject.reverse('hello')).to eq('olleh')
+    end
+  end
+
+  describe '#fizzbuzz' do
+    it 'returns fizz when is n is divisible by 3' do
+      expect(subject.fizzbuzz(3)).to eq('fizz')
+    end
+    it 'returns buzz when is n is divisible by 5' do
+      expect(subject.fizzbuzz(5)).to eq('buzz')
+    end
+    it 'returns fizz when is n is divisible by 3 and 5' do
+      expect(subject.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+    it 'return n as string ' do
+      expect(subject.fizzbuzz(7)).to eq('7')
+    end
+  end
+end
