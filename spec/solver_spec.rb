9f4a4236ef0f9baec16bf62426c8c479b352c0a0
memicelis <elismemic5@gmail.com>
2023-11-16 14:06:15 +0100

    Add test for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,5 +2,10 @@ require 'rspec'
 require_relative '../solver'
 
 describe Solver do
-    
+
+    describe "#factorial" do
+        it 'returns 1 for factorial of 0' do
+            expect(subject.factorial(0)).to eq(1)
+        end
+    end
 end
